24c58a6259850c09d4f27b359f75923ddfc4e71b
Lucas Alia <lukitis2@gmail.com>
2022-12-22 18:45:27 -0300

    creacion de la clase Sucursal y sus demas clases asociadas en la capa entidad

---
+++ CapaEntidad/Localidad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Localidad
+    {
+        public int id_localidad { get; set; }
+        public Provincia objProvincia{ get; set; }
+        public Pais objPais { get; set; }
+        public string descripcion { get; set; }
+    }
+}
---
+++ CapaEntidad/Sucursal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Sucursal
+    {
+        public int id_sucursal{ get; set; }
+        public Localidad objLocalidad { get; set; }
+        public Provincia objProvincia { get; set; }
+        public Pais objPais { get; set; }
+        public string descripcion { get; set; }
+        public int piso { get; set; }
+    }
+}
+
