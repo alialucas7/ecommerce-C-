c5286efd281396a4e7421337c10bc8a3d6c64488
Lucas Alia <lukitis2@gmail.com>
2022-12-22 17:36:13 -0300

    creacion de la clase con el nombre Permiso, en la capa entidad

---
+++ CapaEntidad/Permiso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Permiso
+    {
+        public int id_permiso{ get; set; }
+        public Rol objtRol { get; set; }
+        public string nameMenu { get; set; }
+        public string feharegistro { get; set; }
+    }
+}
