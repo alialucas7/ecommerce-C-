53d206051c82e0e7df0e1ca334b3ad86992bae5b
Lucas Alia <lukitis2@gmail.com>
2022-12-22 18:04:46 -0300

    creacion de la clase Usuario, en la capa entidad

---
+++ CapaEntidad/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Usuario
+    {
+        public int id_usuario { get; set; }
+        public Rol obRol{ get; set; }
+        public string dni{ get; set; }
+        public string name{ get; set; }
+        public string apellido{ get; set; }
+        public string email{ get; set; }
+        public string telefono { get; set; }
+        
+        public bool state{ get; set; }
+        public string feharegistro { get; set; }
+        public string pasword { get; set; }
+    }
+}
