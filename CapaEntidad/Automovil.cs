cbde7027d5397462632189920c9ecaf8169fc73c
Lucas Alia <lukitis2@gmail.com>
2022-12-23 01:32:47 -0300

    Creacion de la clase Automovil y sus clases relacionadas en la capa entidad

---
+++ CapaEntidad/Automovil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Automovil
+    {
+        public int id_automovil{ get; set; }
+        public Sucursal id_sucursal { get; set; }
+        public Modelo objModeloo{ get; set; }
+        public Marca objMarcaa{ get; set; }
+        public int patente { get; set; }
+        public float precio{ get; set; }
+        public int stock { get; set; }
+        public bool estado { get; set; }
+        public string fechaRegistro { get; set; }
+        public int nPuertas { get; set; }
+        public string color { get; set; }
+        public string añoFabricacion { get; set; }
+
+    }
+}
+
---
+++ CapaEntidad/Modelo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Modelo
+    {
+        public int id_modelo{ get; set; }
+        public Marca objMarca { get; set; }
+        public string descripcion{ get; set; }
+    }
+}
+
