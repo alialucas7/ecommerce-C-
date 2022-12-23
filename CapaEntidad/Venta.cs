9d77687db99859e6ce05895390e1ed26e0e84e85
Lucas Alia <lukitis2@gmail.com>
2022-12-23 02:22:09 -0300

    creacion de la clase factura en la capa entidad

---
+++ CapaEntidad/Venta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Venta
+    {
+        public int id_venta { get; set; }
+        public Usuario objUsuario { get; set; }
+        public TipoFactura objTipoFact { get; set; }
+        public string dniCliente { get; set; }
+        public string nameCliente { get; set; }
+        public float montoPago { get; set; }
+        public float montoCambio { get; set; }
+        public float montoTotal { get; set; }
+        public string fechaRegistro { get; set; }
+    }
+}
