09dbf0559ca67a7066974019242d1980f7b3bc83
Lucas Alia <lukitis2@gmail.com>
2023-01-07 03:16:24 -0300

    Creacion de la clase CD_Rol en la capa de datos

---
+++ CapadeDatos/CD_Rol.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapadeDatos;
+using CapaEntidad;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CapadeDatos
+{
+    public class CD_Rol
+    {
+
+
+        public List<Rol> Listar()
+        {
+            List<Rol> lista = new List<Rol>();
+
+            using (SqlConnection conexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    StringBuilder query2 = new StringBuilder();
+                    query2.AppendLine("select * from Rol");
+                   
+
+                    SqlCommand cmd = new SqlCommand(query2.ToString(), conexion);
+                    
+                    cmd.CommandType = CommandType.Text;
+                    conexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Rol()
+                            {
+                                id_rol =  Convert.ToInt32(dr["id_rol"]),
+                                descripcion = dr["descripcion"].ToString()
+
+                                
+
+                            });
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lista = new List<Rol>();
+                }
+
+                return lista;
+            }
+        }
+    }
+}
